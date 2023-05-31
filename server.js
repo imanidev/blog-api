e3f088d1a81572b7d554746f9e6bd61206775c1d
imani.devs <imaniroberts.dev@gmail.com>
2023-05-31 06:40:16 -0400

    removed routes

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // dotenv
 require('dotenv').config();
 
-// path
-const path = require('path');
-const cookieParser = require('cookie-parser');
-
 // express
 const express = require('express');
+const app = express();
 
-// post model
+//load  controllers
+const PostController = require('./controllers/posts');
+app.use('/posts', PostController);
+
+// path
+const path = require('path');
+
+// model
 const Post = require('./models/Post');
 
 // connect to database
 require('./config/database');
-
-//instantiate express
-const app = express();
 
 // cors
 const cors = require('cors');
@@ -25,45 +26,37 @@ const PORT = process.env.PORT || 3001
 
 // other variables
 const logger = require('morgan');
-app.use(logger('dev')); 
+const cookieParser = require('cookie-parser');
 
 
 // middleware
 app.use((req, res, next) => {
-
-    console.log('I run for all routes');
-    next();
+console.log('I run for all routes');
+next();
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(logger('dev')); 
 
-//routes
-const postRoutes = require('./routes/post');
-app.use('/posts', postRoutes);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
 
 //import seed data
-const postData = require('./utilities/data');
+const seedData = require('./utilities/data');
 
 //seed data
 app.get('/seed', async (req,res) => {
   try {
     await Post.deleteMany({});
-    await Post.insertMany(postData);
+    await Post.insertMany(seedData);
     res.send('Posts seeded!');
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error seeding');
+    res.status(500).send('Error in seeding');
   }
 });
-
-
 
 // listener
 app.listen(PORT, () => {
